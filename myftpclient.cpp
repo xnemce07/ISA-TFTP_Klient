b2d27eca113600c367b4ebe1c9734728d15a3e90
Leopold Nemček <leo.nemcek@gmail.com>
2021-10-31 00:22:31 +0200

    Added socket creation and binding. Next step: tftp

--- myftpclient.cpp
+++ myftpclient.cpp
@@ -1,13 +1,13 @@
-
-
-#include <getopt.h>
 #include <iostream>
-#include <regex>
+
 #include "argparser.h"
 
-
-
-
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[])
 {
@@ -15,23 +15,82 @@ int main(int argc, char *argv[])
 
     if (!argparser(&options, argc, argv))
     {
-        std::cout << "UH OH ON ARGPARSE\n";
         return false;
     }
 
     if (!check_options(&options))
     {
-        std::cout << "UH OH ON CHECKOPTIONS\n";
         return false;
     }
 
-    std::cout << "PATH: " << options.path << std::endl;
-    std::cout << "SIZE: " << options.size << std::endl;
-    std::cout << "TIMEOUT: " << options.timeout << std::endl;
-    std::cout << "MODE: " << options.mode << std::endl;
-    std::cout << "IP: " << options.ip << std::endl;
-    std::cout << "PORT: " << options.port << std::endl;
+    //TODO: Randomize tid
+    const char *my_tid = "2323";
+
+    //**************Finding a match and creating a socket + bind******************////
+    struct addrinfo client_hints, *client_servinfo, *p_client;
+    struct addrinfo server_hints, *server_servinfo, *p_server;
+
+    int rv;
+    int sockfd;
+
+    //CLIENT getaddrinfo()
+    memset(&client_hints, 0, sizeof(client_hints));
+    client_hints.ai_family = AF_UNSPEC;
+    client_hints.ai_socktype = SOCK_DGRAM;
+    client_hints.ai_flags = AI_PASSIVE;
+
+    if ((rv = getaddrinfo(NULL, my_tid, &client_hints, &client_servinfo)) != 0)
+    {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
+
+    //SERVER getaddrinfo()
+    memset(&server_hints, 0, sizeof(client_hints));
+    server_hints.ai_family = AF_UNSPEC;
+    server_hints.ai_socktype = SOCK_DGRAM;
+
+    if ((rv = getaddrinfo(options.ip.c_str(), options.port.c_str(), &server_hints, &server_servinfo)) != 0)
+    {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
+
+    for (p_client = client_servinfo; p_client != NULL; p_client = p_client->ai_next)
+    {
+        for (p_server = server_servinfo; p_server != NULL; p_server = p_server->ai_next)
+        {
+            //TODO: check if we also need to compare ai_protocol and ai_socktype
+            if (p_server->ai_family != p_client->ai_family)
+            {
+                perror("Didnt find a match in ai_family.\n");
+                continue;
+            }
+
+            if ((sockfd = socket(p_client->ai_family, p_client->ai_socktype, p_client->ai_protocol)) == -1)
+            {
+                perror("socket() failed.\n");
+                continue;
+            }
+
+            if (bind(sockfd, p_client->ai_addr, p_client->ai_addrlen))
+            {
+                close(sockfd);
+                perror("bind() failed.\n");
+                continue;
+            }
+
+            break;
+        }
+
+        if (p_server != NULL)
+        {
+            break;
+        }
+    }
+
+    freeaddrinfo(client_servinfo);
+    //Here we shoul be able to sendto() and recvfrom() p_server for first request, then with port from their info from recvfrom()
 
     return 0;
 }
-
